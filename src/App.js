02a47de7ed4b269b4960be764c014c693ff30e33
ItsReallyLucifer <itsreallylucifer@gmail.com>
2023-10-02 15:17:23 -0700

    completed app - needs payment plan page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import gptLogo from './assets/chatgpt.svg';
+import gptLogo from './assets/LumenAI.png';
 import addBtn from './assets/add-30.png';
 import msgIcon from './assets/message.svg';
 import home from './assets/home.svg';
@@ -7,25 +7,62 @@ import saved from './assets/bookmark.svg';
 import rocket from './assets/rocket.svg';
 import sendBtn from './assets/send.svg';
 import userIcon from './assets/user-icon.png';
-import gptImgLogo from './assets/chatgptLogo.svg';
-import { useState } from 'react';
+import gptImgLogo from './assets/chatLogo.png';
+import { useEffect, useState, useRef } from 'react';
 import { sendMsgToOpenAI } from './openai';
 
 
 function App() {
+    const msgEnd = useRef(null);
+
     const [input, setInput] = useState("");
     const [messages, setMessages] = useState([
       {
-        text: "I am LumenAI, an AI language model designed to provide information.",
+        text: "I am LumenAI, What may I assist you with?",
         isBot: true, 
       }
   ]);
 
+  useEffect(() => { 
+    msgEnd.current.scrollIntoView();
+  }, [messages]);
+
     const handleSend = async () => {
-    const res = await sendMsgToOpenAI(input);
+      const text = input;
+      setInput('');
+      setMessages([
+        ...messages,
+        {text, isBot: false}
+      ]);
+
+    const res = await sendMsgToOpenAI(text);
     setMessages([
       ...messages,
       { text: input, isBot: false },
+      { text: res, isBot: true }
+    ]);
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      handleSend();
+    }
+  };
+
+  const handleQuery = async (query) => {
+    setInput(query);
+      setMessages([
+        ...messages,
+        {text: query, isBot: false },
+      ]);
+
+    const res = await sendMsgToOpenAI(query);
+
+    setInput('');
+
+    setMessages([
+      ...messages,
+      { text: query, isBot: false },
       { text: res, isBot: true }
     ]);
   }
@@ -35,10 +72,10 @@ function App() {
         <div className='sideBar'>
           <div className='upperSide'>
             <div className='upperSideTop'> <img src={gptLogo} alt='Logo' className='logo' /><span className='brand'>LumenAI</span></div>
-            <button className='midBtn'><img src={addBtn} alt='new chat' className='addBtn' />New Chat</button>
+            <button className='midBtn' onClick={()=>{window.location.reload()}}><img src={addBtn} alt='new chat' className='addBtn' />New Chat</button>
             <div className='upperSideBottom'>
-              <button className='query'><img src={msgIcon} alt='Query' />What is Programming?</button>
-              <button className='query'><img src={msgIcon} alt='Query' />How to use an API?</button>
+              <button className='query' onClick={() => handleQuery("What is Programming?")}><img src={msgIcon} alt='Query' />What is Programming?</button>
+              <button className='query' onClick={() => handleQuery("Population of California?")}><img src={msgIcon} alt='Query' />Population of California?</button>
             </div>
           </div>
           <div className='lowerSide'>
@@ -54,10 +91,11 @@ function App() {
                   <img className='chatImg' src={message.isBot?gptImgLogo:userIcon} alt='' /><p className='txt'>{ message.text }</p>
                 </div>
             )}
+            <div ref={msgEnd}/>
           </div>
           <div className='chatFooter'>
             <div className='inp'>
-              <input type='text' placeholder='Send a message' value={input} onChange={(e)=>{setInput(e.target.value)}}/><button className='send' onClick={handleSend}><img src={sendBtn} alt='Send' /></button>
+              <input type='text' placeholder='Send a message' value={input} onChange={(e)=>{setInput(e.target.value)}} onKeyPress={handleKeyPress}/><button className='send' onClick={handleSend}><img src={sendBtn} alt='Send' /></button>
             </div>
           <p>LumenAI may produce inaccurate results</p>
         </div>
